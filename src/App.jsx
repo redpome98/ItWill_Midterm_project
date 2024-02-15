e136f33430519b5c33806313a75dbc34024c6e64
redpome98 <50163074+redpome98@users.noreply.github.com>
2024-02-15 22:51:39 +0900

    날짜 추가

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,38 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import "./App.css";
 
-function App(position) {
+function App() {
   const [weatherInfo, setWeatherInfo] = useState();
+  const [currentDate, setDate] = useState(new Date());
 
   const WeatherReportCall = (position) => {
     const lat = position.coords.latitude;
     const lon = position.coords.longitude;
     const APIKey = "ee3a80c22900f5a43e517ffa956e525f";
     const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${APIKey}`;
+    //const langUrl = `https://api.openweathermap.org/data/2.5/weather?id=524901&lang=fr&appid=${APIKey}`;
     console.log(url);
+    //console.log(langUrl);
 
     fetch(url)
       .then((response) => response.json())
       .then((data) => setWeatherInfo(data))
       .catch((error) => console.log("Fetching Error", error));
   };
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setDate(new Date());
+    }, 60000);
+
+    return () => {
+      clearInterval(interval);
+    };
+  }, []);
+
+  const year = currentDate.getFullYear();
+  const month = currentDate.getMonth() + 1;
+  const day = currentDate.getDate();
 
   const BtnWeatherReport = () => {
     if (navigator.geolocation) {
@@ -30,9 +47,12 @@ function App(position) {
       <button onClick={BtnWeatherReport}>Report Weather</button>
       {weatherInfo && (
         <div>
-          <h2>Weather Report</h2>
+          <h2>
+            Today is {year}/{month}/{day}
+          </h2>
           <p>Temperature : {(weatherInfo.main.temp - 273.15).toFixed(2)}'C</p>
-          <p>Humid : {weatherInfo.humidty}%</p>
+          <p>Humidity : {weatherInfo.main.humidity}%</p>
+    
         </div>
       )}
     </div>
